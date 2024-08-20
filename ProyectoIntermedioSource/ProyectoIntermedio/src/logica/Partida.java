66cfb0d292c8261a793f7bcaf4d1a86963c7e66a
Jose Erramuspe <erramusperodriguezjosegabriel@gmail.com>
2024-08-20 18:29:43 -0300

    Update Partida.java

--- ProyectoIntermedioSource/ProyectoIntermedio/src/logica/Partida.java
+++ ProyectoIntermedioSource/ProyectoIntermedio/src/logica/Partida.java
@@ -1,5 +1,17 @@
 package logica;
 
 public abstract class Partida {
+    private Date fecha;
+    private String tipoPartida;
+    private String resultado;
+    private ArrayList<String> eventos;
+
+    public Partida(Date fecha, String tipoPartida, String resultado, ArrayList<String> eventos) {
+        this.fecha = fecha;
+        this.tipoPartida = tipoPartida;
+        this.resultado = resultado;
+        this.eventos = new ArrayList<>(eventos);
+    }
+
   
 }
