4056c279ca3f80c793a0a8809b98002f75a9ed2b
MateoMas <mateomaslukinskas@gmail.com>
2024-08-23 19:34:34 -0300

    Update Ejecutor.java

--- ProyectoIntermedio/ProyectoIntermedio/src/presentacion/Ejecutor.java
+++ ProyectoIntermedio/ProyectoIntermedio/src/presentacion/Ejecutor.java
@@ -1,9 +1,12 @@
 package presentacion;
-
+import java.util.Scanner;
 public class Ejecutor {
-
+ Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args) {
-		
+		 boolean continuar = true;
+
+        while (continuar) {
+            mostrarMenu();
 	}
 
 }
