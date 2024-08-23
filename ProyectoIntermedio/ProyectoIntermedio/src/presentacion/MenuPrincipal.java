f42ab8338fc911a1dcaeaa0b4649297abebcff53
MateoMas <mateomaslukinskas@gmail.com>
2024-08-23 20:44:58 -0300

    Update MenuPrincipal.java

--- ProyectoIntermedio/ProyectoIntermedio/src/presentacion/MenuPrincipal.java
+++ ProyectoIntermedio/ProyectoIntermedio/src/presentacion/MenuPrincipal.java
@@ -14,7 +14,7 @@ public class MenuPrincipal {
 		System.out.println("|    1. Crear Personaje    |");
 		System.out.println("|     2. Jugar Partida     |");
 		System.out.println("| 3. Historial de Partidas |");
-		System.out.println("|         4. Salir          |");
+		System.out.println("|         4. Salir         |");
 		System.out.println("|==========================|");
 		int op = entrada.nextInt();
 
