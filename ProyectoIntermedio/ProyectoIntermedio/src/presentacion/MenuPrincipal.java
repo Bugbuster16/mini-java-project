9ad8b09f10275ced4632f47ea5447b22be8f0684
MateoMas <mateomaslukinskas@gmail.com>
2024-08-23 19:27:18 -0300

    Update MenuPrincipal.java

--- ProyectoIntermedio/ProyectoIntermedio/src/presentacion/MenuPrincipal.java
+++ ProyectoIntermedio/ProyectoIntermedio/src/presentacion/MenuPrincipal.java
@@ -2,11 +2,10 @@ package presentacion;
 
 import java.util.Scanner;
 
-public class MenuPrincipal extends Menu {
+public class MenuPrincipal {
 	Scanner entrada = new Scanner (System.in);
 	
-	@Override
-	void mostrarMenu() {
+	public void mostrarMenu() {
 		System.out.println("|==========================|");
 		System.out.println("|=>      Bienvenido      <=|");
 		System.out.println("|==========================|");
@@ -15,6 +14,40 @@ public class MenuPrincipal extends Menu {
 		System.out.println("| 3. Historial de Partidas |");
 		System.out.println("|==========================|");
 		int op = entrada.nextInt();
-		
+
+		  switch (op) {
+            case 1:
+                //pendiente
+                break;
+            case 2:
+                //pendiente
+                break;
+            case 3:
+                mostrarHistorialResumido();
+						System.out.println("|==========================|");
+						System.out.println("|    1.  Ver a detalle     |");
+						System.out.println("|    2. Eliminar partida   |");
+						System.out.println("|    3. Salir              |");
+						System.out.println("|==========================|");
+				  int opcc = entrada.nextInt();
+				  		  switch (opcc) {
+           			 case 1:
+               			 mostrarDetallePartida();
+                			break;
+          			  case 2:
+               			 eliminarPartida();
+              			  break;
+         			   case 3:
+           			     System.out.println("Volviendo al menú...");		  
+			                break;
+           			 default:
+               			 System.out.println("Opción no válida");
+              			  break;
+   			     }
+                break;
+            default:
+                System.out.println("Opción no válida");
+                break;
+        }
 	}
 }
