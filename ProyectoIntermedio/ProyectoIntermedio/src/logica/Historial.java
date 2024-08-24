b704aed2e76229b218817e2fd93ca037034e219f
MateoMas <mateomaslukinskas@gmail.com>
2024-08-24 17:08:45 -0300

    Update Historial.java

--- ProyectoIntermedio/ProyectoIntermedio/src/logica/Historial.java
+++ ProyectoIntermedio/ProyectoIntermedio/src/logica/Historial.java
@@ -7,20 +7,19 @@ public class Historial {
     private ArrayList<Combate> historial;
     private Scanner scanner = new Scanner(System.in);
 
-    public void agregarPartida(Combate combate) {
+    public Historial() {
+        this.historial = new ArrayList<>();
+        this.scanner = new Scanner(System.in);
+    }
+
+    public Historial(ArrayList<Combate> historial, Scanner scanner) {
+        this.historial = historial;
+        this.scanner = scanner;
+    }
+	public void agregarPartida(Combate combate) {
         historial.add(combate);
     }
 
-    public void mostrarHistorialResumido() {
-        if (historial.isEmpty()) {
-            System.out.println("No hay partidas registradas.");
-        } else {
-            System.out.println("Historial de Partidas:");
-            for (int i = 0; i < historial.size(); i++) {
-                System.out.println((i + 1) + ". " + historial.get(i).getResumen());
-            }
-        }
-    }
 
     public void mostrarDetallePartida() {
         System.out.println("¿Qué partida desea ver en detalle?");
@@ -33,7 +32,7 @@ public class Historial {
     }
 
     public void eliminarPartida() {
-        System.out.println("¿Qué partida desea eliminar?");
+        System.out.println("¿Qué partida desa elimienar?");
         int opcion = scanner.nextInt();
         if (opcion > 0 && opcion <= historial.size()) {
             historial.remove(opcion - 1);
@@ -42,4 +41,36 @@ public class Historial {
             System.out.println("Opción inválida.");
         }
     }
+    public void mostrarHistorialResumido() {
+        if (historial.isEmpty()) {
+            System.out.println("No hay partidas registradas.");
+          
+        } else {
+            System.out.println("Historial de Partidas:");
+            for (int i = 0; i < historial.size(); i++) {
+                System.out.println((i + 1) + ". " + historial.get(i).getResumen());
+                System.out.println("|==========================|");
+				System.out.println("|    1.  Ver a detalle     |");
+				System.out.println("|    2. Eliminar partida   |");
+				System.out.println("|    3. Salir              |");
+				System.out.println("|==========================|");
+		  int opcc = scanner.nextInt();
+		  		  switch (opcc) {
+   			 case 1:
+       			 mostrarDetallePartida();
+        			break;
+  			  case 2:
+       			 eliminarPartida();
+      			  break;
+ 			   case 3:
+   			     System.out.println("Volviendo al menú...");		  
+	                break;
+   			 default:
+       			 System.out.println("Opción no válida");
+      			  break;
+		     }
+            }
+        }
+    }
+
 }
