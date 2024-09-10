ce5c2c2607e540cb4b4e0371785456bc2efc742d
Bugbuster16 <152695927+Bugbuster16@users.noreply.github.com>
2024-09-10 17:22:08 -0300

    Update MenuPrincipal.java

--- ProyectoIntermedio/ProyectoIntermedio/src/presentacion/MenuPrincipal.java
+++ ProyectoIntermedio/ProyectoIntermedio/src/presentacion/MenuPrincipal.java
@@ -1,42 +1,52 @@
 package presentacion;
 
-import logica.*;
+import logica.CombatePvP;
+import logica.CombatePvE;
+import logica.Historial;
+import logica.Combate;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MenuPrincipal {
-	Historial historial = new Historial();
 	Scanner entrada = new Scanner (System.in);
 	private boolean continuar=true;
+	private ArrayList<Combate> historial = new ArrayList<>();
+	
 	public void mostrarMenu() {
 		while (continuar) {
 		System.out.println("|==========================|");
 		System.out.println("|=>      Bienvenido      <=|");
 		System.out.println("|==========================|");
-		System.out.println("|    1. Crear Personaje    |");
-		System.out.println("|     2. Jugar Partida     |");
-		System.out.println("| 3. Historial de Partidas |");
-		System.out.println("|         4. Salir         |");
-		System.out.println("|==========================|");
+		System.out.println("|    1. Jugar partida PvP    |");
+		System.out.println("|    2. Jugar partida PvE    |");
+		System.out.println("|  3. Historial de Partidas  |");
+		System.out.println("|          4. Salir          |");
+		System.out.println("|============================|");
 		int op = entrada.nextInt();
 
 		  switch (op) {
-            case 1:
-                //pendiente
-                break;
-            case 2:
-                //pendiente
-                break;
-            case 3:
-                historial.mostrarHistorialResumido();		
-                break;
-            case 4:
-                     System.out.println("¡Gracias por jugar!");
-				  continuar = !continuar;
-				  break;
-            default:
-                System.out.println("Opción no válida");
-                break;
-        }
+            		case 1:
+                		CombatePVP pvp = new CombatePvP;
+				pvp.inicarCombate();
+				historial.add(pvp);
+                		break;
+         		case 2:
+               			CombatePvE pve = new CombatePvE;
+				pve.inicarCombate();
+				historial.add(pve);
+             			break;
+          		case 3:
+            			Historial historia = new Historial(historial, entrada);
+				historia.mostrarHistorialResumido();
+             			break;
+           		case 4:
+                  		System.out.println("¡Gracias por jugar!");
+				continuar = !continuar;
+				 break;
+           		default:
+              			System.out.println("Opción no válida");
+               			break;
+        	}
 	}
 }
 }
